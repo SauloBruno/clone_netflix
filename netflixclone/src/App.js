42ee23db456f8a12cf7a396830a3d1fb2e0dc818
Saulo_Calixto <brunocalisto9@gmail.com>
2022-06-03 16:40:27 -0300

    listas
    exibição parcial dos filmes em formato de lista

--- netflixclone/src/App.js
+++ netflixclone/src/App.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import tmdb from './tmdb';
+import './App.css';
+import Linha_filme from './components/Linha_filme';
+
 
 export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+
+  //quando a tela for carregada ele vai executar a função que eu digitar aqui 
+  useEffect(()=>{
+    const loadAll = async () => {
+      //pegando a lista total
+      let list = await tmdb.getHomeList();
+      setMovieList(list);
+    }
+
+    loadAll();
+  }, []);
+
   return (
-    <div>
-      Olá Mundo!
+    <div className='page'>
+      <section className='lists'>
+        {movieList.map((item, key)=>(
+          <Linha_filme key={key} title={item.title} itens={item.itens} />
+        ))}
+      </section>
     </div>
   );
 }
--- netflixclone/src/tmdb.js
+++ netflixclone/src/tmdb.js
@@ -14,7 +14,7 @@ const API_BASE = 'https://api.themoviedb.org/3';
 
 //função auxiliar que vai dar um fetch na url que eu quero pegar, vai pegar o Json de resultado e retornar ele. 
 //endpoint - url que vamos receber no caso.
-const BasicFetch = async (endpoint) =>{
+const basicFetch = async (endpoint) => {
     const req = await fetch(`${API_BASE}${endpoint}`);
     const json  = await req.json();
     return json;
@@ -23,47 +23,47 @@ const BasicFetch = async (endpoint) =>{
 
 //(método) lista da pagina principal da netflix
 export default {
-    getHomeList:async () =>{
-        return[
+    getHomeList: async () => {
+        return [
             {
                 slug: 'originals',
                 title: 'Originais do Netflix',
-                itens: await BasicFetch(`/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}`)
+                itens: await basicFetch(`/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'trending',
                 title: 'Recomendados para Você',
-                itens: await BasicFetch(`/trending/all/week?language=pt-BR&api_key=${API_KEY}`)
+                itens: await basicFetch(`/trending/all/week?language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'top rated',
                 title: 'Em alta',
-                itens: await BasicFetch(`/movie/top_rated?language=pt-BR&api_key=${API_KEY}`)
+                itens: await basicFetch(`/movie/top_rated?language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'action',
                 title: 'Ação',
-                itens: await BasicFetch(`discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
+                itens: await basicFetch(`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'comedy',
                 title: 'Comédia',
-                itens: await BasicFetch(`discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`)
+                itens: await basicFetch(`/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'horror',
                 title: 'Terror',
-                itens: await BasicFetch(`discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`)
+                itens: await basicFetch(`/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'romance',
                 title: 'Romance',
-                itens: await BasicFetch(`discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`)
+                itens: await basicFetch(`/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'documentary',
                 title: 'Documentários',
-                itens: await BasicFetch(`discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`)
+                itens: await basicFetch(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`)
             },
         ];
     }
